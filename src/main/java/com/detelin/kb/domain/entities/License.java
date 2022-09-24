f4ec4620b17a813eae0c083582085c0b2b0417a2
PetarLalov <petarlalov82@gmail.com>
2022-09-24 19:03:10 +0300

    licenseUpdate

--- src/main/java/com/detelin/kb/domain/entities/License.java
+++ src/main/java/com/detelin/kb/domain/entities/License.java
@@ -3,11 +3,18 @@ package com.detelin.kb.domain.entities;
 import com.detelin.kb.domain.enums.LicenseName;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "license")
+
 public class License extends BaseEntity {
     private LicenseName name;
-
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private List<User> userId;
     public License() {
     }
     @Column(name = "name")
@@ -18,5 +25,34 @@ public class License extends BaseEntity {
 
     public void setName(LicenseName name) {
         this.name = name;
+    }
+    @Column (name = "start_date")
+
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    @Column (name = "end_date")
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    @Column (name = "user_id")
+    @OneToMany(targetEntity = User.class,fetch = FetchType.EAGER)
+
+    public List<User> getUserId() {
+        return userId;
+    }
+
+    public void setUserId(List<User> userId) {
+        this.userId = userId;
     }
 }
